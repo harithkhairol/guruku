858e518e8551c1fdfe681112b778e6a2b3d778ba
ThatGuy <harithkhairol@gmail.com>
2022-01-03 09:54:45 +0800

    update gitignore and feed post

--- resources/views/user/feed.blade.php
+++ resources/views/user/feed.blade.php
@@ -223,6 +223,18 @@
 
 @section('script')
 
+    <script src="{{ asset('vendor/jquery.form.min.js') }}"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script> -->
+
+    @if(Auth::check())
+
+        @include('modals.post')
+        @include('modals.photo')
+        @include('modals.video')
+        @include('modals.post-edit')
+        @include('modals.post-delete')
+
+    @endif
 
     <script>
 
